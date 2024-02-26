fe7e90e494fcb59b4d35b194ae76bcb3d91fc78b
issaali <issa.alali.991@gmail.com>
2024-02-26 09:37:20 +0100

    create connection with DB

---
+++ server/db/db.js
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log(err.stack));
--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 import express from 'express';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
+// import cookieParser from 'cookie-parser';
+// import contactRouter from './routes/contactRouter.js';
+import './db/db.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+
+
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 
 app.use(express.json());
-app.use(cookieParser()); 
+// app.use(cookieParser()); 
 
 // ROUTES
+// app.use('/contacts', contactRouter);
 
 // Error Handler
 

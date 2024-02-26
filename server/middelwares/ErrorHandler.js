49da8bfdbab3ffe8a9d7543fe92e7d23359d6924
jb <workingj@pm.me>
2024-02-26 11:52:22 +0100

    fix errorhandler middleware typo

--- server/middelwares/ErrorHandler.js
+++ server/middelwares/ErrorHandler.js
@@ -1,5 +1,5 @@
 export const errorHandler = (error, req, res, next) => {
     const statusCode = error.statusCode || 500;
-    const message = eeror.message || 'Error from ErrorHandler in middleware !!!';
+    const message = error.message || 'Error from ErrorHandler in middleware !!!';
     res.status(statusCode).json({ message: message });
 };

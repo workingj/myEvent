83d90d9601c360094b20ade5c274faae243cf7a4
Fara <f_karamizadeh@yahoo.com>
2024-02-26 11:08:38 +0100

    little changes

--- server/server.js
+++ server/server.js
@@ -1,22 +1,18 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import { errorHandler } from "./middlewares/ErrorHandler.js";
 // import cookieParser from 'cookie-parser';
 // import contactRouter from './routes/contactRouter.js';
-import './db/db.js';
-import dotenv from 'dotenv';
+import "./db/db.js";
+import dotenv from "dotenv";
 dotenv.config();
-
-
-
-
-
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 
 app.use(express.json());
-// app.use(cookieParser()); 
+// app.use(cookieParser());
 
 // ROUTES
 // app.use('/contacts', contactRouter);

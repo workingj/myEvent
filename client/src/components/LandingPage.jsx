10790336e0c3412f617d5bc9c522649d4cb94bd6
jb <workingj@pm.me>
2024-03-13 11:08:44 +0100

    fix margin top

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,7 +1,7 @@
 function LandingPage() {
   return (
     <>
-      <div className="container flex justify-center items-center rounded-xl shadow-xl shadow-gray-500  max-w-4xl max-h-1/3 mt-20 border bg-white bg-opacity-80 m-auto">
+      <div className="container flex justify-center items-center rounded-xl shadow-xl shadow-gray-500  max-w-4xl max-h-1/3 mt-20 mb-20 border bg-white bg-opacity-80 m-auto">
         <div className="  rounded-xl shadow-xl shadow-gray-500 ">
           <img className="rounded-xl shadow-2xlxl shadow-gray-500 "
             src="https://res.cloudinary.com/duaxo0f43/image/upload/v1709895603/cool-images/intro_pm1wsp.gif"
